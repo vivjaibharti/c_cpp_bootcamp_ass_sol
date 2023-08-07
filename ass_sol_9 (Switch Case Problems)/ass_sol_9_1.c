eac7aea6350a0a03797f3ad150778217518da786
vivjaibharti <vivjaibharti@gmail.com>
2023-08-07 14:29:21 +0530

    Assignment Updated (07-08-2023)

--- ass_sol_9 (Switch Case Problems)/ass_sol_9_1.c
+++ ass_sol_9 (Switch Case Problems)/ass_sol_9_1.c
@@ -5,8 +5,14 @@ int main()
 {
 
  int month,flag;
- printf("Enter the month's number to find its no. days:");
+ printf("Enter the month's number to find its no. of days:");
  scanf("%d",&month);
+ while(month>12 || month<1)
+ {
+    printf("Wrong Input\n");
+    printf("Enter the month's number to find its no. of days:");
+    scanf("%d",&month);
+ }
  if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12)
  {
     flag=0;
@@ -30,11 +36,7 @@ int main()
         case -1:
             printf("%dth month contains 30 days.",month);
             break;
-        default:
-            printf("Wrong Input");
-            break;
     }
-
  return 0;
 
 }
