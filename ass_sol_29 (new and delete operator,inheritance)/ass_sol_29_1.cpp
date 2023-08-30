6e29143f883d15717b39841b65c72d741e3a94c7
vivjaibharti <vivjaibharti@gmail.com>
2023-08-30 23:26:49 +0530

    -Assignment Updated (30-08-2023)

---
+++ ass_sol_29 (new and delete operator,inheritance)/ass_sol_29_1.cpp
@@ -0,0 +1,21 @@
+/* Topic:1. Define a class Person with instance members name and age. Also define member
+functions setName(), setAge(), getName(), getAge(). Now define class Employee by
+inheriting Person class. In the Employee class define empid and salary as instance
+members. Also define setEmpid, setSalary, getEmpid, getSalary
+*/
+#include <iostream>
+using namespace std;
+
+class 
+{
+    
+};
+
+
+int main()
+{
+    
+    
+    
+    return 0;
+}
